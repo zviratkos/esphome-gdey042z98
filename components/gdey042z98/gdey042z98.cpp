048ccb76f59a78edf01b04415ea118650f1272b4
Zviratkos <github@stribrsky.cz>
2026-03-23 23:31:45 +0100

    Send all white to refresh

--- components/gdey042z98/gdey042z98.cpp
+++ components/gdey042z98/gdey042z98.cpp
@@ -119,15 +119,30 @@ void GDEY042Z98::do_send_() {
     uint16_t w = this->partial_w_;
     uint16_t h = this->partial_h_;
 
-    // DIAGNOSTIKA: poslat celý B/W buffer (celá plocha) bez červené
-    this->set_partial_ram_area_(0, 0, GDEY042Z98_WIDTH, GDEY042Z98_HEIGHT);
+    // Poslat bílou do previous RAM (0x26) pro celou oblast – vymaže červenou a ghosting
+    this->set_partial_ram_area_(x, y, w, h);
+    this->send_command_(0x26);
+    this->dc_pin_->digital_write(true);
+    this->enable();
+    for (uint16_t row = y; row < y + h; row++)
+      for (uint16_t col = x; col < x + w; col += 8)
+        this->transfer_byte(0xFF);  // bílá = žádná červená, žádný ghosting
+    this->disable();
+
+    // Poslat nový obsah do current RAM (0x24)
+    this->set_partial_ram_area_(x, y, w, h);
     this->send_command_(0x24);
     this->dc_pin_->digital_write(true);
     this->enable();
-    for (size_t i = 0; i < BUFFER_SIZE; i++)
-      this->transfer_byte(this->bw_buffer_[i]);
+    for (uint16_t row = y; row < y + h; row++) {
+      for (uint16_t col = x; col < x + w; col += 8) {
+        size_t byte_idx = (row * (GDEY042Z98_WIDTH / 8)) + (col / 8);
+        this->transfer_byte(this->bw_buffer_[byte_idx]);
+      }
+    }
     this->disable();
 
+    // Partial refresh
     this->send_command_(0x22);
     this->send_data_(0xF7);
     this->send_command_(0x20);
