40e053075dd6c514361527ebbb2dbdeb6e576a6b
Zviratkos <github@stribrsky.cz>
2026-03-23 23:36:31 +0100

    Invert red register

--- components/gdey042z98/gdey042z98.cpp
+++ components/gdey042z98/gdey042z98.cpp
@@ -119,16 +119,6 @@ void GDEY042Z98::do_send_() {
     uint16_t w = this->partial_w_;
     uint16_t h = this->partial_h_;
 
-    // Poslat bílou do previous RAM (0x26) pro celou oblast – vymaže červenou a ghosting
-    this->set_partial_ram_area_(x, y, w, h);
-    this->send_command_(0x26);
-    this->dc_pin_->digital_write(true);
-    this->enable();
-    for (uint16_t row = y; row < y + h; row++)
-      for (uint16_t col = x; col < x + w; col += 8)
-        this->transfer_byte(0xFF);  // bílá = žádná červená, žádný ghosting
-    this->disable();
-
     // Poslat nový obsah do current RAM (0x24)
     this->set_partial_ram_area_(x, y, w, h);
     this->send_command_(0x24);
@@ -142,9 +132,22 @@ void GDEY042Z98::do_send_() {
     }
     this->disable();
 
-    // Partial refresh
+    // Poslat stejný obsah do previous RAM (0x26) – bez inverze, jen B/W
+    this->set_partial_ram_area_(x, y, w, h);
+    this->send_command_(0x26);
+    this->dc_pin_->digital_write(true);
+    this->enable();
+    for (uint16_t row = y; row < y + h; row++) {
+      for (uint16_t col = x; col < x + w; col += 8) {
+        size_t byte_idx = (row * (GDEY042Z98_WIDTH / 8)) + (col / 8);
+        this->transfer_byte(this->bw_buffer_[byte_idx]);  // stejná data, bez inverze
+      }
+    }
+    this->disable();
+
+    // Partial B/W refresh – 0xC7
     this->send_command_(0x22);
-    this->send_data_(0xF7);
+    this->send_data_(0xC7);
     this->send_command_(0x20);
     ESP_LOGD(TAG, "Partial refresh spuštěn (x=%d y=%d w=%d h=%d)...",
              x, y, w, h);
